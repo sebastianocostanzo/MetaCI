0d0e71cddf4c1c51aa5b1851ef017403a2543f8c
Chris Polcyn <cpolcyn@salesforce.com>
2017-06-30 16:23:11 -0700

    use non-paginated view for build detail

--- mrbelvedereci/build/views.py
+++ mrbelvedereci/build/views.py
@@ -19,7 +19,7 @@ class ApiBuildDetail(generics.RetrieveAPIView):
     serializer_class = BuildSerializer
 
     def get_queryset(self):
-        return view_queryset(self.request)
+        return view_queryset(self.request, multiple=False)
 
 
 class ApiBuildList(generics.ListAPIView):
